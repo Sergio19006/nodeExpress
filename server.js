1778348f717b7b704145fbf2f936f5d366de0601
sergio19006 <sergiodphn@gmail.com>
2019-07-02 16:59:52 +0100

    Añadiendo

--- server.js
+++ server.js
@@ -8,5 +8,9 @@ app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
+app.get('/login', (req, res) => {
+  res.send('Hello you are in the index');
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
