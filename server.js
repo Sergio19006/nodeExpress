c2804e5f97d7b98df1d18642bda076a6f3c46674
sergio19006 <sergiodphn@gmail.com>
2019-07-02 18:57:29 +0100

    A tope

--- server.js
+++ server.js
@@ -3,9 +3,33 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const users = [
+  {
+    id:1, 
+    name:'juan'
+  },
+  {
+    id:2,
+    name:'sergio'
+  }
+]
+
+
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  console.log("Aparcao: ",req.headers);
+  res.json([{message: 'Hello world'}]);
+});
+
+app.get('/users', (req, res) => {
+  console.log("Aparcao: ",req.headers);
+  res.json([{users: users}]);
+});
+
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const user = users.find(u => userId == u.id);
+  res.json(user);
 });
 
 app.get('/login', (req, res) => {
